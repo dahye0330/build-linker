f479cc375297a793bff7a4e1651afd9f4db161a0
dahye park <vdahyes@naver.com>
2024-12-10 21:52:33 +0900

    first commit

---
+++ src/main/java/me/dahye/buildlinker/BuildLinkerApplication.java
@@ -0,0 +1,13 @@
+package me.dahye.buildlinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildLinkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildLinkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dahye/buildlinker/BuildLinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dahye.buildlinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildLinkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
